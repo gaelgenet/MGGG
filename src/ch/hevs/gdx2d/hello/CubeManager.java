4fc59d8013d9fcaad92731a594b4d52c653017e7
gael.genet <gael.genet@students.hevs.ch>
2017-05-18 17:12:40 +0200

    add method to create random rectangle with height and width. add time between each creation of the rectangle accelerate the speed of the game with in relation with the time

--- src/ch/hevs/gdx2d/hello/CubeManager.java
+++ src/ch/hevs/gdx2d/hello/CubeManager.java
@@ -12,51 +12,69 @@ import ch.hevs.gdx2d.lib.utils.Logger;
 public class CubeManager {
 
 	Vector<Cube> cubes = new Vector<Cube>();
-	
+
 	float dt;
+	float speedTime;
 	int posx = 1000;
 	int posy = 0;
 	int width;
-	int high;
-	int speed = -1;
-	float speedTime;
-	
+	int heigh;
+	float speed = -4;
+	float variablespeed = 3;
+
 	Color c = Color.WHITE;
 
 	Random r = new Random();
-	
+
 	void generatecube() {
+
 		dt += Gdx.graphics.getDeltaTime();
-		
-	//	Logger.log("Current time " + dt);
-		if (dt > 3) {
-		//	Logger.log("Generating a CUBBEEEEE!");
+
+		// Logger.log("Current time " + dt);
+		if (dt > variablespeed) {
+			// Logger.log("Generating a CUBBEEEEE!");
 			dt = 0;
-			width = (int) (Math.min(Math.random() * 1000, 500));
-			high = (int) (Math.min(Math.random() * 1000, 600));
-			
-			cubes.add(new Cube(posx, posy, width, high, new Color(r.nextInt())));
+			width = (int) (Math.random() * 1000);
+			if (width < 200 || width > 500) {
+				width = 500;
+			}
+			heigh = (int) (Math.random() * 1000);
+
+			if (heigh < 200 || heigh > 700) {
+				heigh = 500;
+			}
+
+			cubes.add(new Cube(posx, posy, width, heigh, new Color(r.nextInt())));
 		}
 
 	}
 
-	public void drawCubes(GdxGraphics g) {
+	void distroyCube() {
+		if (cubes.get(0).posx < -300) {
+			//Logger.log("cube"+cubes.get(0)+"is destroyed");
+			cubes.remove(0);
+		}
+	}
+
+	public void moveCube(GdxGraphics g) {
 		speedTime += Gdx.graphics.getDeltaTime();
-	//	Logger.log("pouet"+speedTime);
-		
-		if(speedTime>5){
-			speed -= 2;
+		//Logger.log("pouet" + speedTime);
+
+		if (speedTime > 7) {
+			speed -= 0.5;
+			variablespeed -= 0.2;
 			speedTime = 0;
 		}
-		
-		for(Cube c: cubes){
+
+		for (Cube c : cubes) {
 			c.draw(g);
 			c.move(speed);
 		}
-		Logger.log("salut" + speed + "");
+		//Logger.log("salut" + speed + "");
 	}
 
 	public void generateInitialCubes() {
+		cubes.add(new Cube(posx, posy, 400, 400, new Color(r.nextInt())));
 
 	}
 
