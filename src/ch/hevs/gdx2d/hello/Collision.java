30b44804c14eaf67544b0cf293487c3892e1025a
gael.genet <gael.genet@students.hevs.ch>
2017-05-19 15:29:01 +0200

    add syso

--- src/ch/hevs/gdx2d/hello/Collision.java
+++ src/ch/hevs/gdx2d/hello/Collision.java
@@ -1,11 +1,12 @@
 package ch.hevs.gdx2d.hello;
 
-public class Colision {
+public class Collision {
 	Cube cube;
 	Bonhomme bonhomme;
 	boolean colision = false;
 
 	public void colision() {
+		System.out.println(colision);
 		if (bonhomme.squarre.contains(cube.rectangle)) {
 			colision = true;}
 		else{
