712d79460d471b26394a356d5814347de312ef84
gael.genet <gael.genet@students.hevs.ch>
2017-05-23 15:24:10 +0200

    modify collision methode with enum

--- src/ch/hevs/gdx2d/hello/Collision.java
+++ src/ch/hevs/gdx2d/hello/Collision.java
@@ -1,17 +1,18 @@
 package ch.hevs.gdx2d.hello;
 
 public class Collision {
-	Cube cube;
-	Bonhomme bonhomme;
-	boolean colision = false;
+	
+	public enum CollisionType {NONE, TOP, LEFT};
 
-	public void colision() {
-		System.out.println(colision);
-		if (bonhomme.squarre.contains(cube.rectangle)) {
-			colision = true;}
-		else{
-			colision = false;
+	public static CollisionType collides(Cube cube, Bonhomme bonhomme) {
+		if (bonhomme.squarre.overlaps(cube.rectangle)) {
+			if (bonhomme.squarre.y >= cube.rectangle.height) {
+				return CollisionType.TOP;
+			} else {
+				return CollisionType.LEFT;
 			}
-	}
+		}
 
+		return CollisionType.NONE;
+	}
 }
