864cf19dee6e827f2d5edb2809791891009aea25
Marco Gonçalves Freitas <Marco Gonalves Freitas>
2017-06-01 16:11:53 +0200

    amelirorer

--- src/ch/hevs/gdx2d/hello/Collision.java
+++ src/ch/hevs/gdx2d/hello/Collision.java
@@ -5,23 +5,31 @@ import ch.hevs.gdx2d.components.geometry.Point;
 public class Collision {
 
 	public enum CollisionType {
-		NONE, TOP, LEFT
+		NONE, TOP, LEFT, END
 	};
 
 	public static CollisionType collides(Cube cube, Bonhomme bonhomme) {
 		Point basDroit = new Point((int) bonhomme.squarre.x + (Bonhomme.SPRITE_WIDTH / 2),
 				(int) bonhomme.squarre.y - (Bonhomme.SPRITE_HEIGHT / 2));
 
+		if ((bonhomme.squarre.y - (Bonhomme.SPRITE_HEIGHT / 2)) >= (cube.rectangle.height / 2)-20 && (bonhomme.squarre.y - (Bonhomme.SPRITE_HEIGHT / 2)) < (cube.rectangle.height / 2)-2 ) {
 		if ((bonhomme.squarre.x + (Bonhomme.SPRITE_WIDTH / 2)) >= cube.rectangle.x - (cube.rectangle.width / 2)) {
-			if ((bonhomme.squarre.y - (Bonhomme.SPRITE_HEIGHT / 2)) >= (cube.rectangle.height / 2)-25 && (bonhomme.squarre.y - (Bonhomme.SPRITE_HEIGHT / 2)) < (cube.rectangle.height / 2) ) {
 				return CollisionType.TOP;
 			}
-			else if(bonhomme.squarre.y - (Bonhomme.SPRITE_HEIGHT / 2) < (cube.rectangle.height / 2)){
-			return CollisionType.LEFT;}
-
+		
 		}
 		
+		if ((bonhomme.squarre.x + (Bonhomme.SPRITE_WIDTH / 2)) >= cube.rectangle.x - (cube.rectangle.width / 2)) {
+		if(bonhomme.squarre.y - (Bonhomme.SPRITE_HEIGHT / 2) < (cube.rectangle.height / 2)-20){
+			return CollisionType.LEFT;}}
+		
+		
+		
+  
+		
+		
 		return CollisionType.NONE;
+		
 
 		// if (bonhomme.squarre.y - (Bonhomme.SPRITE_HEIGHT / 2) <=
 		// cube.rectangle.height / 2) {
