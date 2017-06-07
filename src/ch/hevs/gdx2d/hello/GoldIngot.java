05a7bb37a2460fb620cb3a0b6e4acf67706d7d2e
Marco Gonçalves Freitas <Marco Gonalves Freitas>
2017-06-07 19:03:46 +0200

    add rectangle

--- src/ch/hevs/gdx2d/hello/GoldIngot.java
+++ src/ch/hevs/gdx2d/hello/GoldIngot.java
@@ -1,5 +1,7 @@
 package ch.hevs.gdx2d.hello;
 
+import com.badlogic.gdx.math.Rectangle;
+
 import ch.hevs.gdx2d.components.bitmaps.BitmapImage;
 import ch.hevs.gdx2d.lib.GdxGraphics;
 import ch.hevs.gdx2d.lib.interfaces.DrawableObject;
@@ -7,8 +9,11 @@ import ch.hevs.gdx2d.lib.interfaces.DrawableObject;
 public class GoldIngot implements DrawableObject {
 
 	 static BitmapImage ingot;
-	public void onInit() {
-	ingot = new BitmapImage("data/images/gold.png");}
+	 Rectangle square;
+	 
+	 
+	 
+	 
 	
 	
 	int posX;
@@ -16,15 +21,27 @@ public class GoldIngot implements DrawableObject {
 	int ale;
 	
 
+	public void onInit() {
+		ingot = new BitmapImage("data/images/gold.png");
+		square = new Rectangle();
+		square.height= 25;
+		square.width =25;
+		
+		}
 	
 	public void drawingPositionIngot(CubeManager c){
 		
 		posY = (int) (c.cubes.get(0).rectangle.height/2 + ingot.getImage().getHeight()/4);
-		posX = (int) (c.cubes.get(0).rectangle.x + c.cubes.get(0).rectangle.width/4);}
+		posX = (int) (c.cubes.get(0).rectangle.x + c.cubes.get(0).rectangle.width/4);
+		square.x = posX;
+		square.y = posY;
+		}
+		
 	
 	@Override
 	public void draw(GdxGraphics g) {
 		g.drawTransformedPicture(posX, posY, 0, 0.25f, ingot);
+		
 		
 	}
 
