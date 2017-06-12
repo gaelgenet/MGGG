990195ba0d0261d261874d5c3db7c2c034e52510
Marco Gonçalves Freitas <Marco Gonalves Freitas>
2017-06-12 21:35:08 +0200

    update class bird with mini-dragon oder bat

--- src/ch/hevs/gdx2d/hello/Bird.java
+++ src/ch/hevs/gdx2d/hello/Bird.java
@@ -22,16 +22,27 @@ public class Bird implements DrawableObject {
 	static Spritesheet sprites;
 
 	// The size of each sprite in the sheet
+	/**
+	 * 
 	static int SPRITE_WIDTH = 109;
 	static int SPRITE_HEIGHT = 100;
+	 * 
+	static int SPRITE_WIDTH = 64;
+	static int SPRITE_HEIGHT = 64;
+
+	 */
+	
+	static int SPRITE_WIDTH = 96;
+	static int SPRITE_HEIGHT = 96;
+	
 
 	// The currently selected sprite for animation
 	int currentFrame = 0;
-	int textureY = 0;
+	int textureY = 1;
 
 	// Animation related parameters
 	float dt = 0;
-	int nFrames = 3;
+	int nFrames = 4;
 	double FRAME_TIME = 0.15; // Duration of each frame
 	int nChange = 1;
 	int direction = 1;
@@ -51,7 +62,9 @@ public class Bird implements DrawableObject {
 	public Bird(int posX, int posY) {
 		this.posX = posX;
 		this.posY = posY;
-		sprites = new Spritesheet("data/images/spritbird.png", SPRITE_WIDTH, SPRITE_HEIGHT);
+		sprites = new Spritesheet("data/images/batD.png", SPRITE_WIDTH, SPRITE_HEIGHT);
+//		sprites = new Spritesheet("data/images/bat2.png", SPRITE_WIDTH, SPRITE_HEIGHT);
+//		sprites = new Spritesheet("data/images/spritbird.png", SPRITE_WIDTH, SPRITE_HEIGHT);
 	}
 
 	/**
@@ -67,18 +80,18 @@ public class Bird implements DrawableObject {
 			currentFrame += 1;
 			if (currentFrame == nFrames) {
 				currentFrame = 0;
-				textureY += 1;
+				//textureY += 1;
 				nChange++;
 				if (nChange % 3 == 0) {
 					direction *= -1;
 				}
 			}
 
-			if (textureY == 2 && currentFrame == 2) {
-				textureY = 0;
-				currentFrame = 0;
-
-			}
+//			if (textureY == 2 && currentFrame == 2) {
+//				textureY = 0;
+//				currentFrame = 0;
+//
+//			}
 		}
 		if (posX < -580) {
 			posX = 980;
